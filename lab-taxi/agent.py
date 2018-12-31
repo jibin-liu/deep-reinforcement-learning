c58a642b68c676286e7b605dfd8801025a850353
Jibin Liu <liujibin320@gmail.com>
2018-12-30 18:28:14 -0800

    move my files into the forked repo

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,17 +1,26 @@
 import numpy as np
+import random
 from collections import defaultdict
+
 
 class Agent:
 
-    def __init__(self, nA=6):
+    def __init__(self, nA=6, alpha=1, gamma=1,
+                 epsilon_init=1, epsilon_decay=0.999):
         """ Initialize agent.
 
         Params
         ======
         - nA: number of actions available to the agent
         """
+        print(f'alpha={alpha}, gamma={gamma}, ', end='')
+        print(f'epsilon_init={epsilon_init}, epsilon_decay={epsilon_decay}.')
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = epsilon_init
+        self.epsilon_decay = epsilon_decay
+        self.alpha = alpha
+        self.gamma = gamma
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,10 +33,13 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        if random.random() > self.epsilon:
+            return np.argmax(self.Q[state])
+        else:
+            return random.choice(np.arange(self.nA))
 
     def step(self, state, action, reward, next_state, done):
-        """ Update the agent's knowledge, using the most recently sampled tuple.
+        """ Update the agent's knowledge using expected_sarsa, using the most recently sampled tuple.
 
         Params
         ======
@@ -37,4 +49,36 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+
+        expected_sarsa = self.get_expected_sarsa(next_state) if not done else 0
+        approx_return = reward + self.gamma * expected_sarsa
+        self.Q[state][action] += self.alpha * (approx_return - self.Q[state][action])
+
+        if done:  # update epsilon
+            self.epsilon *= self.epsilon_decay
+    
+    def get_expected_sarsa(self, state):
+        action_values = self.Q[state]
+        best_action = np.argmax(action_values)
+        p = np.full(action_values.shape, self.epsilon / self.nA)
+        p[best_action] += 1 - self.epsilon
+        return np.dot(action_values, p)
+
+    def step_q_learning(self, state, action, reward, next_state, done):
+        """ Update the agent's knowledge using q_learning, using the most recently sampled tuple.
+
+        Params
+        ======
+        - state: the previous state of the environment
+        - action: the agent's previous choice of action
+        - reward: last reward received
+        - next_state: the current state of the environment
+        - done: whether the episode is complete (True or False)
+        """
+
+        max_q_of_next_state = max(self.Q[next_state]) if not done else 0
+        approx_return = reward + self.gamma * max_q_of_next_state
+        self.Q[state][action] += self.alpha * (approx_return - self.Q[state][action])
+
+        if done:  # update epsilon
+            self.epsilon *= self.epsilon_decay
